ed72b62eb96bdbd30c774aecdc87600ba0d92e91
maudro <maudro29@gmail.com>
2019-10-11 11:58:37 +0300

    complited

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function multiply(first, second) {
-  // your solution
+ module.exports = function multiply(first, second) {
+  let firstArray = first.split("").reverse();
+  let secondArray = second.split("").reverse();
+  let steck = [];
+
+  for (let firstCounter = 0; firstCounter < firstArray.length; firstCounter++) {
+    for (let secondCounter = 0; secondCounter < secondArray.length; secondCounter++) {
+      let multi = Number(firstArray[firstCounter]) * Number(secondArray[secondCounter]);
+      if (steck[firstCounter + secondCounter] == null) {
+        steck[firstCounter + secondCounter] = multi;
+      } else {steck[firstCounter + secondCounter] += multi; }
+      
+      if (steck[firstCounter + secondCounter] >= 10) {
+        if (steck[firstCounter + secondCounter + 1] == null) {
+          steck[firstCounter + secondCounter + 1] = Math.floor(steck[firstCounter + secondCounter] / 10);
+        } else {steck[firstCounter + secondCounter + 1] += Math.floor(steck[firstCounter + secondCounter] / 10);}
+        steck[firstCounter + secondCounter] = steck[firstCounter + secondCounter] % 10;
+      }      
+    }
+  }
+  return steck.reverse().join("");
 }
